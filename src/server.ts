8d4703dadd4a1eb58cd9c8214e58e54d06003d75
Zichao Zang <zang.code@gmail.com>
2023-07-12 19:47:09 +0100

    Complete tasks from "testing apis" assignment

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
+import * as dotenv from "dotenv";
+const environment = process.env.NODE_ENV || "dev";
+dotenv.config({ path: `.env.${environment}` });
+
 import { app } from "./app";
 import { populateDummyData } from "./database/database_seed";
 
-const environment = process.env.NODE_ENV || "dev";
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 console.log(`🌍 Running in ${environment} environment`);
 
 app.listen(PORT, () => {
-	console.log(`🚂 Express started on port ${PORT}`);
+    console.log(`🚂 Express started on port ${PORT}`);
 
-	// Seed the database with some data
-	if (environment === "dev") {
-		populateDummyData();
-	}
+    // Seed the database with some data
+    if (environment === "dev") {
+        populateDummyData();
+    }
 });
